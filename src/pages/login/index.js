f48778c19d954e78f8bcc8ae619631267c464afe
victorelf <51044012+victorelf@users.noreply.github.com>
2019-08-04 16:44:18 +0800

    Update index.js

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -11,8 +11,8 @@ class Login extends PureComponent {
 			return (
 			<LoginWrapper>
 				<LoginBox>
-					<Input placeholder='账号' innerRef={(input) => {this.account = input}}/>
-					<Input placeholder='密码' type='password' innerRef={(input) => {this.password = input}}/>
+					<Input placeholder='账号' ref={(input) => {this.account = input}}/>
+					<Input placeholder='密码' type='password' ref={(input) => {this.password = input}}/>
 					<Button onClick={() => this.props.login(this.account, this.password)}>登录</Button>
 				</LoginBox>
 			</LoginWrapper>
